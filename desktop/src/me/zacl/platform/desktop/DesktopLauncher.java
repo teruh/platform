9c9d450257e79a9846e87404c8695d27a9a683bb
teruh <zachclarkbyte@gmail.com>
2020-04-06 11:11:21 -0400

    Add project files

---
+++ desktop/src/me/zacl/platform/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.zacl.platform.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.zacl.platform.Platform;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Platform(), config);
+	}
+}
