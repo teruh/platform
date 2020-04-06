b7b1efc01c4afc0387b6bcf9ac83377cb4da8b17
teruh <zachclarkbyte@gmail.com>
2020-04-06 12:41:48 -0400

    Add PlayScreen class

--- core/src/me/zacl/platform/Platform.java
+++ core/src/me/zacl/platform/Platform.java
@@ -1,33 +1,29 @@
 package me.zacl.platform;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import me.zacl.platform.screen.PlayScreen;
 
-public class Platform extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class Platform extends Game {
+   public SpriteBatch batch;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+   @Override
+   public void create () {
+      batch = new SpriteBatch();
+      setScreen(new PlayScreen(this));
+   }
+
+   @Override
+   public void render () {
+      super.render();
+   }
+
+   @Override
+   public void dispose () {
+      batch.dispose();
+      super.dispose();
+   }
 }
